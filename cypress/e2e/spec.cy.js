d22929483e91112b83c611d93a837dd4b192c431
Ayush Praharaj <69159222+AyushPrj@users.noreply.github.com>
2024-04-12 00:12:05 -0600

    Added CD Pipeline for Cypress Testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('passes', () => {
+    // cy.visit('http://127.0.0.1:5500/')
+    cy.visit('https://vebly.netlify.app/')
+    cy.get('#section_controls2').click();
+    cy.get('.delete-btn').each(($el, index, $list) => {
+      cy.wrap($el).click({force: true});
+    });
+    cy.get('#section_controls1').click();
+    cy.get('#section_addition').click({force: true});
+    cy.should('exist')
+  })
+})
+
+
