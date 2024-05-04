2d16244fcc95656d27632a13a84e65979c05f97d
Ayush Praharaj <69159222+AyushPrj@users.noreply.github.com>
2024-05-03 23:22:09 -0400

    Changing Cypress testing script

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,11 +1,13 @@
 describe('template spec', () => {
-    it('passes', () => {
-      cy.visit('https://vebly.netlify.app/')
-      cy.wait(1000)
-      // Then simulate the clicks
-      cy.get('#tablet-view').click();
-      cy.get('#canvas').should('have.class', 'tablet');
-      cy.get('#mobile-view').click();
-      cy.get('#canvas').should('have.class', 'mobile');
-    })
+  it('passes', () => {
+    // cy.visit('http://127.0.0.1:5500/')
+    cy.visit('https://vebly.netlify.app/')
+    cy.get('#section_controls2').click();
+    cy.get('.delete-btn').each(($el, index, $list) => {
+      cy.wrap($el).click({force: true});
+    });
+    cy.get('#section_controls1').click();
+    cy.get('#section_addition').click({force: true});
+    cy.should('exist')
   })
+})
